e78a17248e6161e8666fe66e3f5d528babdec684
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-01-25 11:36:56 +0530

    Verify test with H2 throws error

--- h2-testing/src/test/java/com/sivalabs/tcdemo/PromotionRepositoryWithH2Test.java
+++ h2-testing/src/test/java/com/sivalabs/tcdemo/PromotionRepositoryWithH2Test.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.dao.DataAccessException;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest
 class PromotionRepositoryWithH2Test {
@@ -38,6 +40,9 @@ class PromotionRepositoryWithH2Test {
     @Test
     void shouldCreateProductIfNotExist() {
         Promotion promotion = new Promotion(null, 1L, BigDecimal.valueOf(25));
-        promotionRepository.upsert(promotion);
+        //promotionRepository.upsert(promotion);
+
+        assertThatThrownBy(() -> promotionRepository.upsert(promotion))
+                .isInstanceOf(DataAccessException.class);
     }
 }
